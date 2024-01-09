5cd49a3968fdfff5fd35b2311a6c60b170ccb359
Shamindi1211 <it21203558@my.sliit.lk>
2024-01-09 22:16:41 +0530

    css

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React ,{ useState } from "react";
+
 import "./home.css";
 import photo from "./img/hmphto.png";
 import fb from "./img/fb.png";
@@ -6,8 +7,36 @@ import googal from "./img/g.png";
 import git from "./img/git.png";
 import linkdin from "./img/linkdin.png";
 import Nav from "../Nav/Nav";
+import first from "./img/1.png";
+import sec from "./img/2.png";
+import third from "./img/3.png";
+import forth  from "./img/4.png";
+import fifth from "./img/5.png";
+import seven from "./img/6.png";
+import eight from "./img/7.png";
+import nine from "./img/8.png";
+
+
 
 const Home = () => {
+
+
+  const [hovered, setHovered] = useState(false);
+  const [projectInfo, setProjectInfo] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleHover = (title, description) => {
+    setHovered(!hovered);
+    setProjectInfo({ title, description });
+  };
+
+  const handleHoverOff = () => {
+    setHovered(false);
+  };
+
+
   return (
     <div>
       <Nav/>
@@ -72,6 +101,143 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+
+
+     <div className="section-title-zero">    
+    <div className="section-title-first">
+      <h2 className="section-title">My Projects</h2>
+      <div className="projects-section">
+      
+          <div className="project-box" onMouseEnter={() => handleHover("First Project", "Description for first project")} onMouseLeave={handleHoverOff}>
+  <img src={first} alt="logo-icon" className="project-image" />
+  {hovered && (
+    <div className="project-popup">
+      <h3>{projectInfo.title}</h3>
+      <p>{projectInfo.description}</p>
+      <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+    </div>
+  )}
+</div>
+          
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={sec} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+          </div>
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={third} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+
+
+          </div>
+          
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={forth} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              
+              <a href="URL_TO_YOUR_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+                <button className="github-button">GitHub Repo</button>
+              </a>
+               </div>
+          )}
+          </div>
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={fifth} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+          </div>
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={seven} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+          </div>
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={eight} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+          </div>
+
+
+          <div className="project-box" onMouseEnter={() => handleHover("She Budget Savvy", "For women, the money is important")}
+          onMouseLeave={handleHoverOff}>
+           < img src={nine} alt="logo-icon" className="project-image" />
+            {hovered && (
+            <div className="project-popup">
+              <h3>{projectInfo.title}</h3>
+              <p>{projectInfo.description}</p>
+              <a href="URL_TO_YOUR_FIRST_GITHUB_REPO" target="_blank" rel="noopener noreferrer" className="github-link">
+        <button className="github-button">GitHub Repo</button>
+      </a>
+            </div>
+          )}
+          </div>
+
+          <div className="project-box">
+            
+          </div>
+
+          <div className="project-box">
+            
+          </div>
+          
+        </div>
+
+
+        </div>
+        </div>
     </div>
   );
 };
